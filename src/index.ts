88103043030512bf9aa257aef6898ddd6f6200a3
Lukas Köhler <lukas.koehler@ueberbit.de>
2023-08-11 22:07:03 +0200

    Add Design

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,18 @@ const hateService = new HateService();
 hateService.getTotalHaters().then(response => {
     console.log(response);
 })
+window.addEventListener("DOMContentLoaded", ()=>{
+    let btnprivacy : HTMLButtonElement | null = document.querySelector(".btn-privacy");
+    if (btnprivacy) {
+        btnprivacy.addEventListener("click", ()=> {
+            let banner : HTMLDivElement | null = document.querySelector(".privacy-banner");
+            if (banner) {
+                banner.classList.add("hidden");
+                let btnentry : HTMLButtonElement | null = document.querySelector(".btn-addentry");
+                if (btnentry) {
+                    btnentry.disabled = false;
+                }
+            }
+        });
+    }
+});
